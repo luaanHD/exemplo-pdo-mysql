ac8d2853d0baf58573c37a2c6aaff910c7569168
luaanHD <luantoniolo94@gmail.com>
2022-11-11 03:09:44 +0000

    Barra de Navegação

--- includes/header.php
+++ includes/header.php
@@ -10,4 +10,22 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Biblioteca</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/index.php">Gêneros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/list_livros.php" aria-current="page">Livros</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+    <br>
     <main class="container">
